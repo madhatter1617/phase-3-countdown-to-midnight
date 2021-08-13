faf87c06438b054ce1ce37eab59a0c46f50ad18d
Rosa <rosarenee@att.net>
2021-08-13 09:37:50 -0500

    commit tree 9c0fb5cb20b900f65f7904794c4f04cad6ff5b31

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(seconds_TM)
+while seconds_TM > 0
+  puts "#{seconds_TM} SECOND(S)!"
+  seconds_TM -= 1
 end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_TM)
+    while seconds_TM > 0
+      puts "#{seconds_TM} SECOND(S)!"
+      sleep(1)
+      seconds_TM -= 1
+    end
+    "HAPPY NEW YEAR!"
+    end
